bff51599b651eaf42c700d1de2d973a3f349beab
Anton.b <46520333+silex-rr@users.noreply.github.com>
2023-04-05 12:24:15 +0300

    remove system folder

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    'node_modules/daisyui/dist/**/*.js',
+    'node_modules/react-daisyui/dist/**/*.js'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+}
