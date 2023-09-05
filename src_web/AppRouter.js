7063112ad7a1f58328b0daf8be50a638e7d08071
Polina <polina@blsv.space>
2023-09-05 17:35:54 +0200

    a route ro "admin" was added; {AdminPanelProvider} and AdminPanelRouter were imported.

--- src_web/AppRouter.js
+++ src_web/AppRouter.js
@@ -3,6 +3,8 @@ import {IdentityProvider} from "../src_shared/identity/contexts/IdentityContext"
 import RequireAuth from "../src_shared/identity/utils/RequireAuth";
 import PasswordBrokerRouter from "./passwordBroker/routers/PasswordBrokerRouter";
 import AuthContainer from "./identity/components/AuthContainer";
+import { AdminPanelProvider } from "./adminPanel/contexts/AdminPanelContext";
+import AdminPanelRouter from "./adminPanel/routers/AdminPanelRouter";
 
 const AppRouter = () => {
     return (
@@ -26,6 +28,18 @@ const AppRouter = () => {
                         </IdentityProvider>
                     }
                 />
+                <Route 
+                    path="/admin/*"
+                    element={
+                        <IdentityProvider>
+                            <RequireAuth>
+                                <AdminPanelProvider>
+                                    <AdminPanelRouter />
+                                </AdminPanelProvider>
+                            </RequireAuth>
+                        </IdentityProvider>
+                    }
+                 />
             </Routes>
         </BrowserRouter>
     )
