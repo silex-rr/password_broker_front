897bc36dd79cd9816094a1eff4f6e29d9cd8ed96
Polina <polina@blsv.space>
2023-09-06 11:02:18 +0200

    cleared from all console.log; loading block was moved to a separate component (AdminPanelLoading); added a unique key to each tr.

--- src_web/adminPanel/components/user_control/UserControl.js
+++ src_web/adminPanel/components/user_control/UserControl.js
@@ -6,26 +6,22 @@ import {
     USER_CONTROL_LOADED,
     USER_CONTROL_NOT_LOADED,
     USER_CONTROL_LOADING } from "./UserControlStatus"
-import {ClockLoader} from "react-spinners";
+import AdminPanelLoading from "../AdminPanelLoading";
 
 
 const UserControl = () => {
     const {getUsers} = useContext(IdentityContext)
-    getUsers().then((data) => {console.log(typeof getUsers, data)})
 
     const [userControlStatus, setUserControlStatus] = useState(USER_CONTROL_REQUIRE_LOADING)
     const [userControlData, setUserControlData] = useState([])
 
     useEffect(() => {
-        console.log(userControlStatus)
         if (userControlStatus === USER_CONTROL_LOADED
             || userControlStatus === USER_CONTROL_LOADING
         ) {return}
         setUserControlStatus(USER_CONTROL_LOADING)
-        console.log('before the promise')
         getUsers().then((users) => {
-            console.log('a')
-            setUserControlData(users)
+            setUserControlData(users.data)
             setUserControlStatus(USER_CONTROL_LOADED)
         })
     }, [setUserControlData, setUserControlStatus, userControlStatus])
@@ -33,13 +29,16 @@ const UserControl = () => {
     const users = []
     if (userControlStatus === USER_CONTROL_LOADED) {
         for (let i = 0; i<userControlData.length; i++) {
-            console.log(userControlData[i])
             users.push(userControlData[i])
         }
     } 
     return (
         <div className="overflow-x-auto">
-            <table className="table table-xs">
+             {userControlStatus != USER_CONTROL_LOADED &&
+                <AdminPanelLoading />
+            }  
+            {userControlStatus === USER_CONTROL_LOADED &&
+             <table className="table table-xs">
                 <thead>
                 <tr>
                     <th></th>
@@ -49,24 +48,8 @@ const UserControl = () => {
                 </tr>
                 </thead> 
                 <tbody>
-                    {userControlStatus != USER_CONTROL_LOADED &&
-                        <tr key="empty_group_history">
-                            <td colSpan="100%" className="bg-slate-700 text-slate-100 text-center">
-                                <div className="w-full py-2 flex items-center justify-center">
-                                    <ClockLoader
-                                        color="#e2e8f0"
-                                        size={18}
-                                        aria-label="Loading Spinner"
-                                        data-testid="loader"
-                                        speedMultiplier={1}
-                                    />
-                                    <span className="px-1">loading...</span>
-                                </div>
-                            </td>
-                        </tr>
-                    }  
                     {users.map((user, index) => (
-                        <tr>
+                        <tr key={index}>
                             <td>{index+1}</td>
                             <td>{user.email}</td> 
                             <td>{user.name}</td> 
@@ -74,7 +57,7 @@ const UserControl = () => {
                         </tr>
                     ))}
                 </tbody> 
-            </table>
+            </table>}
         </div>
     )
 }
