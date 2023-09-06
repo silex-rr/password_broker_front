df6929f2693542798352d6d4d08c2313e96c5283
Polina <polina@blsv.space>
2023-09-06 11:03:04 +0200

    created; contains the logic to havigate without reloading a page.

---
+++ src_web/adminPanel/components/AdminPanelLeftMenuNavigation.js
@@ -0,0 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
+const AdminPanelLeftMenuNagivation = (props) => {
+    const navigate = useNavigate()
+    const path = props.path
+    const name = props.name
+
+    const handleNavigation = (path) => {
+        return navigate(path)
+
+    }
+    return <h3 onClick={() => {handleNavigation(path)}}>{name}</h3>
+
+}
+
+export default AdminPanelLeftMenuNagivation
