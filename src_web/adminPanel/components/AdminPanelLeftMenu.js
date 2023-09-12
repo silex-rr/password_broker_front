072a9e9d6c6410cb4ac6e06faa93f3a2e7967495
Polina <polina@blsv.space>
2023-09-12 13:26:29 +0200

    menu is ul now with tailwind properties.

--- src_web/adminPanel/components/AdminPanelLeftMenu.js
+++ src_web/adminPanel/components/AdminPanelLeftMenu.js
@@ -8,15 +8,17 @@ const AdminPanelLeftMenu = () => {
             <div className="basis-1/4 text-slate-400 pr-1">
                 <div className="w-full bg-slate-200 px-3 text-slate-800 py-1">{title}</div>
             </div>
-            <div className="py-5 px-2 relative">
-                <AdminPanelLeftMenuNagivation path={'/admin/user_control/'} name={'User Control'}/>
-            </div>
-            <div className="py-5 px-2 relative">
-                <AdminPanelLeftMenuNagivation path={'/admin/settings/'} name={'Settings'} />
-            </div>
-            <div className="py-5 px-2 relative">
-                <AdminPanelLeftMenuNagivation path={'/admin/logs/'} name={'Logs'} />
-            </div>
+            <ul className="menu">
+                <li className="py-5 px-2 relative">
+                    <AdminPanelLeftMenuNagivation path={'/admin/user_control/'} name={'User Control'}/>
+                </li>
+                <li className="py-5 px-2 relative">
+                    <AdminPanelLeftMenuNagivation path={'/admin/settings/'} name={'Settings'} />
+                </li>
+                <li className="py-5 px-2 relative">
+                    <AdminPanelLeftMenuNagivation path={'/admin/logs/'} name={'Logs'} />
+                </li>
+            </ul>
         </section>
     )
 }
