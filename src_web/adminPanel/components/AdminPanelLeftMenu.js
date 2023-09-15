125f101853071ee22df01f0920c595728eefc99a
Polina <polina@blsv.space>
2023-09-15 10:15:23 +0200

    styles changes.

--- src_web/adminPanel/components/AdminPanelLeftMenu.js
+++ src_web/adminPanel/components/AdminPanelLeftMenu.js
@@ -9,13 +9,13 @@ const AdminPanelLeftMenu = () => {
                 <div className="w-full bg-slate-200 px-3 text-slate-800 py-1">{title}</div>
             </div>
             <ul className="menu">
-                <li className="py-5 px-2 relative">
+                <li className="py-2 px-2 relative">
                     <AdminPanelLeftMenuNagivation path={'/admin/user_control/'} name={'User Control'}/>
                 </li>
-                <li className="py-5 px-2 relative">
+                <li className="py-2 px-2 relative">
                     <AdminPanelLeftMenuNagivation path={'/admin/settings/'} name={'Settings'} />
                 </li>
-                <li className="py-5 px-2 relative">
+                <li className="py-2 px-2 relative">
                     <AdminPanelLeftMenuNagivation path={'/admin/logs/'} name={'Logs'} />
                 </li>
             </ul>
