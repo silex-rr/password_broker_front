3250c76f0933b6c0f05d40ef09e7560fd769976c
Polina <polina@blsv.space>
2023-09-19 15:44:54 +0200

    created, contains 'user_update".

---
+++ src_web/adminPanel/components/user_control/UserControlNagivation.js
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom";
+
+const UserControlNavigation = (props) => {
+    const navigate = useNavigate()
+    const path = props.path
+    const name = props.name
+    
+    const handleNavigation = (path) => {
+        return navigate(path)
+    }
+
+    return (
+        <div onClick={() => {handleNavigation(path)}}>{name}</div>
+    )
+}
+
+export default UserControlNavigation
