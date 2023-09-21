77c957bf003846643b93a79eab7bccbcac9f8634
Polina <polina@blsv.space>
2023-09-21 12:48:36 +0200

    added styles for mobile devices.

--- src_web/adminPanel/components/AdminPanelLeftMenu.js
+++ src_web/adminPanel/components/AdminPanelLeftMenu.js
@@ -4,11 +4,11 @@ const AdminPanelLeftMenu = () => {
     const title = 'Admin panel elements';
 
     return (
-        <section className=" bg-slate-900">
+        <section className="bg-slate-900 portrait:w-full">
             <div className="basis-1/4 pr-1 text-slate-400">
                 <div className="w-full bg-slate-200 px-3 py-1 text-slate-800">{title}</div>
             </div>
-            <ul className="menu">
+            <ul className="menu flex flex-col portrait:flex-row">
                 <li className="relative px-2 py-2">
                     <AdminPanelLeftMenuNagivation path={'/admin/user_control/'} name={'User Control'} />
                 </li>
