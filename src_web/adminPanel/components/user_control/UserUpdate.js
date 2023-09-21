740230d907b144ce371add0fd926b1c6850a599c
Polina <polina@blsv.space>
2023-09-21 12:04:09 +0200

    submit button styles.

--- src_web/adminPanel/components/user_control/UserUpdate.js
+++ src_web/adminPanel/components/user_control/UserUpdate.js
@@ -3,6 +3,7 @@ import { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import AppContext from '../../../AppContext';
 import AdminPanelLoading from '../AdminPanelLoading';
+import { FaCheck, FaXmark } from 'react-icons/fa6'
 
 const UserUpdate = props => {
     const { userID, userIDparam } = useParams();
@@ -13,10 +14,12 @@ const UserUpdate = props => {
         username: '',
         email: '',
     });
-    const [updateStatus, setUpdateStatus] = useState('')
+    const [updateStatus, setUpdateStatus] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     const handleSubmit = async e => {
         e.preventDefault();
+        setIsLoading(true)
         // console.log(e);
         try {
             console.log('putting', userID, formData);
@@ -26,6 +29,7 @@ const UserUpdate = props => {
             // console.log('error', error);
             setUpdateStatus('error')
         }
+        setIsLoading(false)
     };
 
     const handleNameChange = event => {
@@ -99,19 +103,51 @@ const UserUpdate = props => {
                         />
                     </div>
                     {updateStatus === 'error' &&
-                        <p>An error uccored, try again</p>}
+                        <p className='flex flex-row justify-center'> <FaXmark /> An error uccored, try again</p>}
                     {updateStatus === 'updated' &&
-                        <p>The User was successfully updated</p>}
+                        <p className='flex flex-row justify-center'> <FaCheck /> The User was successfully updated</p>}
                     <button
-                        className="edit-button m-3 basis-1/4 bg-green-500 p-2 font-bold text-black 
-                            hover:bg-green-600 hover:font-extrabold"
-                        onClick={handleSubmit}>
-                        Save
+                        type="submit"
+                        className={`submit-button m-3 bg-green-500 p-2 font-bold text-black hover:bg-green-600 hover:font-extrabold ${isLoading ? 'opacity-50 cursor-not-allowed' : ''
+                            }`}
+                        disabled={isLoading}
+                        onClick={handleSubmit}
+                    >
+                        {isLoading ? (
+                            <div className="flex flex-row justify-center items-center">
+                                <svg
+                                    className="animate-spin h-5 w-5 mr-3"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <circle
+                                        className="opacity-25"
+                                        cx="12"
+                                        cy="12"
+                                        r="10"
+                                        stroke="currentColor"
+                                        strokeWidth="4"
+                                    />
+                                    <path
+                                        className="opacity-75"
+                                        fill="currentColor"
+                                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.373A8 8 0 0012 20v-4.373h-6z"
+                                    />
+                                </svg>
+                                Processing...
+                            </div>
+                        ) : (
+                            'Save'
+                        )}
                     </button>
                     <button
-                        className="clear-button m-3 basis-1/4 bg-red-400 p-2 font-bold text-black 
-                            hover:bg-red-500 hover:font-extrabold"
-                        onClick={handleClear}>
+                        className={`clear-button m-3 bg-red-400 p-2 font-bold text-black 
+                            hover:bg-red-500 hover:font-extrabold ${isLoading ? 'opacity-50 cursor-not-allowed' : ''
+                            }`}
+                        onClick={handleClear}
+                        disabled={isLoading}
+                    >
                         Clear changes
                     </button>
                 </div>
