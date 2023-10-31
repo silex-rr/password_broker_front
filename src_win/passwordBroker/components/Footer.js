789915d5af412d9b153784c92e8966f44a290c8e
Anton.b <46520333+silex-rr@users.noreply.github.com>
2023-10-31 22:19:57 +0300

    Show offline database bar only for system administrators

--- src_win/passwordBroker/components/Footer.js
+++ src_win/passwordBroker/components/Footer.js
@@ -1,17 +1,17 @@
 import {View, Text} from 'react-native-windows';
 import tw from 'twrnc';
-import React from 'react';
+import React, {useContext} from 'react';
 import OfflineDatabase from './Footer/OfflineDatabase';
+import IdentityContext from '../../../src_shared/identity/contexts/IdentityContext';
 
 const Footer = () => {
+    const {userIsAdmin} = useContext(IdentityContext);
     return (
         <View style={tw`bg-slate-700 text-slate-300 w-full py-2 px-10 flex justify-between flex-row`}>
             <View style={tw``}>
                 <Text>activity:</Text>
             </View>
-            <View style={tw``}>
-                <OfflineDatabase />
-            </View>
+            <View style={tw``}>{userIsAdmin ? <OfflineDatabase /> : ''}</View>
         </View>
     );
 };
