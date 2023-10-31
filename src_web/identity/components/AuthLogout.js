fd47a213b891973e333e515ad475c0cb6cb3fb60
Anton.b <46520333+silex-rr@users.noreply.github.com>
2023-10-31 19:19:38 +0300

    Unload UserApplication after logout

--- src_web/identity/components/AuthLogout.js
+++ src_web/identity/components/AuthLogout.js
@@ -2,19 +2,16 @@ import {useNavigate} from 'react-router-dom';
 import React, {useContext, useEffect} from 'react';
 import IdentityContext from '../../../src_shared/identity/contexts/IdentityContext';
 import UserApplicationContext from '../../../src_shared/identity/contexts/UserApplicationContext';
-import {DATABASE_MODE_OFFLINE} from '../../../src_shared/identity/constants/DatabaseModeStates';
 
 const AuthLogout = () => {
     const identityContext = useContext(IdentityContext);
-    const {databaseMode, switchDatabaseToOnline} = useContext(UserApplicationContext);
+    const {databaseMode, userApplicationUnload} = useContext(UserApplicationContext);
     const navigate = useNavigate();
     const {logout} = identityContext;
     useEffect(() => {
         logout(navigate, databaseMode);
-        if (databaseMode === DATABASE_MODE_OFFLINE) {
-            switchDatabaseToOnline();
-        }
-    }, [navigate, logout, databaseMode, switchDatabaseToOnline]);
+        userApplicationUnload();
+    }, [navigate, logout, databaseMode, userApplicationUnload]);
     return (
         <div className="w-full rounded md:flex ">
             <div className="rounded-lg bg-white px-12 py-24">
--- src_win/identity/components/AuthLogout.js
+++ src_win/identity/components/AuthLogout.js
@@ -4,19 +4,16 @@ import IdentityContext from '../../../src_shared/identity/contexts/IdentityConte
 import {Text, View} from 'react-native-windows';
 import tw from 'twrnc';
 import UserApplicationContext from '../../../src_shared/identity/contexts/UserApplicationContext';
-import {DATABASE_MODE_OFFLINE} from '../../../src_shared/identity/constants/DatabaseModeStates';
 
 const AuthLogout = () => {
     const identityContext = useContext(IdentityContext);
-    const {databaseMode, switchDatabaseToOnline} = useContext(UserApplicationContext);
+    const {databaseMode, userApplicationUnload} = useContext(UserApplicationContext);
     const navigate = useNavigate();
     const {logout} = identityContext;
     useEffect(() => {
         logout(navigate, databaseMode);
-        if (databaseMode === DATABASE_MODE_OFFLINE) {
-            switchDatabaseToOnline();
-        }
-    }, [logout, navigate, databaseMode, switchDatabaseToOnline]);
+        userApplicationUnload();
+    }, [logout, navigate, databaseMode, userApplicationUnload]);
     return (
         <View style={tw``}>
             <Text style={tw`text-4xl text-slate-700 text-center`}>Logout</Text>
