78a53531a183104604b9733ad4674612c5be7e50
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-01-17 12:49:59 +0300

    Refactor AdminPanel components structure
    Components structure in the AdminPanel has been refactored. In particular, 'React.Fragment', 'MainLeftBlock' and 'MainBodyBlock' components have been introduced, and 'AdminPanelRightMenu' has been moved. These changes will improve the modularity and maintainability of the code.

--- src_web/adminPanel/components/AdminPanelMainBody.js
+++ src_web/adminPanel/components/AdminPanelMainBody.js
@@ -1,13 +1,21 @@
+import React from 'react';
 import AdminPanelLeftMenu from './AdminPanelLeftMenu';
 import AdminPanelRightMenu from './AdminPanelRightMenu';
-
+import MainLeftBlock from '../../common/MainLeftBlock';
+import MainBodyBlock from '../../common/MainBodyBlock';
 const AdminPanelMainBody = props => {
     return (
-        <main className="mb-auto flex flex-grow flex-row flex-wrap">
-            <AdminPanelLeftMenu />
-            <AdminPanelRightMenu>{props.children}</AdminPanelRightMenu>
-        </main>
+        // <main className="mb-auto flex flex-grow flex-row flex-wrap">
+        <React.Fragment>
+            <MainLeftBlock>
+                <AdminPanelLeftMenu />
+            </MainLeftBlock>
+            <MainBodyBlock>
+                <AdminPanelRightMenu>{props.children}</AdminPanelRightMenu>
+            </MainBodyBlock>
+        </React.Fragment>
+
+        // </main>
     );
 };
-
 export default AdminPanelMainBody;
