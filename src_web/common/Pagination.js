f31c79993d0a83f2bc21b2e2d49d08dd5efcb5cf
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-01-17 13:35:02 +0300

    Update pagination button styles and behavior
    Included a condition to hide pagination when there's only one page. Also, updated the button styles to enhance visibility and introduced a new text color property to improve UX. Adapted the code to use this new color setup, improving overall readability and accessibility.

--- src_web/common/Pagination.js
+++ src_web/common/Pagination.js
@@ -1,9 +1,15 @@
+import React from 'react';
+
 const PaginationButton = ({currentPage = 1, lastPage = 1, handlePagination}) => {
+    if (lastPage <= 1) {
+        return '';
+    }
+    const textColour = '!text-slate-200 disabled:!text-slate-400 ';
     return (
         <div className="flex justify-center p-3">
             <div className="join">
                 <button
-                    className="btn btn-outline join-item"
+                    className={`btn btn-outline join-item btn-sm ${textColour}`}
                     disabled={currentPage === 1}
                     onClick={() => {
                         handlePagination(currentPage - 1);
@@ -11,30 +17,32 @@ const PaginationButton = ({currentPage = 1, lastPage = 1, handlePagination}) => 
                     «
                 </button>
                 {currentPage > 1 && (
-                    <button className="btn btn-outline join-item" onClick={() => handlePagination(1)}>
+                    <button
+                        className={`btn btn-outline join-item btn-sm ${textColour}`}
+                        onClick={() => handlePagination(1)}>
                         1
                     </button>
                 )}
                 {currentPage > 2 && (
-                    <button className="btn btn-outline join-item" disabled={true}>
+                    <button className={`btn btn-outline join-item btn-sm ${textColour}`} disabled={true}>
                         ...
                     </button>
                 )}
                 <button
-                    className="btn btn-outline join-item"
+                    className={`btn btn-outline join-item btn-sm ${textColour}`}
                     onClick={() => {
                         handlePagination(currentPage);
                     }}>
                     {currentPage}
                 </button>
-                {currentPage != lastPage && (
-                    <button className="btn btn-outline join-item" disabled={true}>
+                {currentPage !== lastPage && (
+                    <button className={`btn btn-outline join-item btn-sm ${textColour}`} disabled={true}>
                         ...
                     </button>
                 )}
-                {currentPage != lastPage && (
+                {currentPage !== lastPage && (
                     <button
-                        className="btn btn-outline join-item"
+                        className={`btn btn-outline join-item btn-sm ${textColour}`}
                         onClick={() => {
                             handlePagination(lastPage);
                         }}>
@@ -46,7 +54,7 @@ const PaginationButton = ({currentPage = 1, lastPage = 1, handlePagination}) => 
                     onClick={() => {
                         handlePagination(currentPage + 1);
                     }}
-                    className="btn btn-outline join-item">
+                    className={`btn btn-outline join-item btn-sm ${textColour}`}>
                     »
                 </button>
             </div>
