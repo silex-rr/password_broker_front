96b632aaffa202b607ca579ff3f73fbf9571dabc
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-01-17 12:42:03 +0300

    Refactor layout components for better reusability
    The layout styling originally defined in the 'MainLeftMenu', 'MainBody', and 'SearchResult' components has been moved to two new common components, 'MainLeftBlock' and 'MainBodyBlock'

--- src_web/passwordBroker/components/PasswordBrokerSearchContainer.js
+++ src_web/passwordBroker/components/PasswordBrokerSearchContainer.js
@@ -2,12 +2,18 @@ import Container from '../../common/Container';
 import MainLeftMenu from './MainLeftMenu';
 import React from 'react';
 import SearchResult from './SearchResult';
+import MainLeftBlock from '../../common/MainLeftBlock';
+import MainBodyBlock from '../../common/MainBodyBlock';
 
 const PasswordBrokerContainer = () => {
     return (
         <Container>
-            <MainLeftMenu />
-            <SearchResult />
+            <MainLeftBlock>
+                <MainLeftMenu />
+            </MainLeftBlock>
+            <MainBodyBlock>
+                <SearchResult />
+            </MainBodyBlock>
         </Container>
     );
 };
