d79b2cd4d7d96b8d9d1ea32df14f7d0ff5392bd1
Polina <polina@blsv.space>
2024-01-22 12:15:39 +0400

    @react-native extention was removed.

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,11 @@ module.exports = {
     root: true,
     plugins: ['prettier'],
     rules: {
-        'prettier/prettier': ['error', {endOfLine: 'auto'}],
+        'prettier/prettier': ['error', { endOfLine: 'auto' }],
         'max-len': ['error', 120],
         'react/jsx-no-styles-inline': 'off',
         'react-native/no-inline-styles': 0,
         // 'indent': ['error', 4],
     },
-    extends: ['@react-native', 'plugin:prettier/recommended'],
+    extends: ['plugin:prettier/recommended'],
 };
