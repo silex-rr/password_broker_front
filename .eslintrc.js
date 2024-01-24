f5e8c4639359f5f2a8fd33e470fd39306ef9f4c1
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-01-24 12:14:02 +0300

    Update ESLint configuration for React
    This commit updates the ESLint configuration to support React-based linting. Apart from enabling JSX syntax, new rules specific to React are added and the 'react' plugin is included. This enhances code quality checks for React components.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,21 @@
 module.exports = {
     root: true,
-    plugins: ['prettier'],
+    plugins: ['prettier', 'react'],
     rules: {
         'prettier/prettier': ['error', {endOfLine: 'auto'}],
         'max-len': ['error', 120],
         'react/jsx-no-styles-inline': 'off',
-        'react-native/no-inline-styles': 0,
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error',
+        'react/prop-types': 'off',
         // 'indent': ['error', 4],
     },
-    extends: ['plugin:prettier/recommended'],
+    extends: ['plugin:prettier/recommended', 'plugin:react/recommended'],
     parserOptions: {
         ecmaVersion: 2020, // or 2018 or 2017
         sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true, // Includes JSX syntax
+        },
     },
 };
--- src_win/.eslintrc.js
+++ src_win/.eslintrc.js
@@ -4,17 +4,23 @@ module.exports = {
     parserOptions: {
         ecmaVersion: 2020, // or 2018 or 2017
         sourceType: 'module',
+        ecmaFeatures: {
+            jsx: true, // Includes JSX syntax
+        },
         requireConfigFile: true,
         babelOptions: {
             configFile: './../babel.config.js',
         },
     },
-    plugins: ['prettier'],
+    plugins: ['prettier', 'react'],
     rules: {
         'prettier/prettier': ['error', {endOfLine: 'auto'}],
         'max-len': ['error', 120],
         'react/jsx-no-styles-inline': 'off',
         'react-native/no-inline-styles': 0,
+        'react/jsx-uses-react': 'error',
+        'react/jsx-uses-vars': 'error',
+        'react/prop-types': 'off',
         // 'indent': ['error', 4],
     },
     extends: ['@react-native', 'plugin:prettier/recommended'],
