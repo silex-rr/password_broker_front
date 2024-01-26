5d2d4854f8cea01cddcb10936b2bcb813d6f57fd
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-01-26 13:49:59 +0300

    Integrate GlobalContextProvider in AppRouter
    Added GlobalContextProvider to AppRouter.js. This allows the GlobalContext to encapsulate the IdentityContextProvider and UserApplicationContextProvider. As a result, user activity logging and data sharing across different components is improved.

--- src_win/AppRouter.js
+++ src_win/AppRouter.js
@@ -6,35 +6,38 @@ import React, {useContext} from 'react';
 import AppContext from './AppContext';
 import IdentityContextProvider from '../src_shared/identity/contexts/IdentityContextProvider';
 import UserApplicationContextProvider from '../src_shared/identity/contexts/UserApplicationContextProvider';
+import GlobalContextProvider from '../src_shared/common/contexts/GlobalContextProvider';
 
 const AppRouter = () => {
     const {hostURL, getClientId, offlineDatabaseService, appTokensService} = useContext(AppContext);
 
     return (
         <NativeRouter>
-            <IdentityContextProvider
-                hostURL={hostURL}
-                tokenMode={true}
-                getClientId={getClientId}
-                appTokensService={appTokensService}
-                AppContext={AppContext}>
-                <UserApplicationContextProvider
-                    getClientId={getClientId}
+            <GlobalContextProvider>
+                <IdentityContextProvider
                     hostURL={hostURL}
-                    offlineDatabaseService={offlineDatabaseService}>
-                    <Routes>
-                        <Route
-                            path={'/*'}
-                            element={
-                                <RequireAuth>
-                                    <PasswordBrokerRouter />
-                                </RequireAuth>
-                            }
-                        />
-                        <Route path="/identity/*" element={<IdentityRouter />} />
-                    </Routes>
-                </UserApplicationContextProvider>
-            </IdentityContextProvider>
+                    tokenMode={true}
+                    getClientId={getClientId}
+                    appTokensService={appTokensService}
+                    AppContext={AppContext}>
+                    <UserApplicationContextProvider
+                        getClientId={getClientId}
+                        hostURL={hostURL}
+                        offlineDatabaseService={offlineDatabaseService}>
+                        <Routes>
+                            <Route
+                                path={'/*'}
+                                element={
+                                    <RequireAuth>
+                                        <PasswordBrokerRouter />
+                                    </RequireAuth>
+                                }
+                            />
+                            <Route path="/identity/*" element={<IdentityRouter />} />
+                        </Routes>
+                    </UserApplicationContextProvider>
+                </IdentityContextProvider>
+            </GlobalContextProvider>
         </NativeRouter>
     );
 };
