edd0301ecdd87d5b537fd3ba208f6893e3178fc5
Polina <polina@blsv.space>
2024-02-01 12:44:52 +0400

    added handleNewTime function; added password block.

--- src_web/adminPanel/components/backup/Backup.js
+++ src_web/adminPanel/components/backup/Backup.js
@@ -4,14 +4,37 @@ const Backup = () => {
     const [backupTime, setBackupTime] = useState("12:00");
     const [isBackupOn, setIsBackupOn] = useState(false);
     const [sendConfirmation, setSendConfirmation] = useState(false);
+    const [backupPassword, setBackupPassword] = useState(false);
     const [email, setEmail] = useState("");
+    const [password, setPassword] = useState('')
 
-    const handleNewTime = () => {
-        // Perform the POST request with the selected backupTime
-        console.log("POST request with backupTime:", backupTime);
-        // Include logic for sending confirmation email if sendConfirmation is true
+    const handleNewTime = async () => {
         if (sendConfirmation) {
-            console.log(`Sending confirmation email to: ${email}`);
+            const data = {
+                schedule: [backupTime],
+                enable: isBackupOn,
+                email_enable: sendConfirmation,
+                email: sendConfirmation ? email : null,
+                archive_password: backupPassword ? password : null,
+            };
+
+            try {
+                const response = await fetch("/system/api/setting/backupSetting/backup", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify(data),
+                });
+
+                if (response.ok) {
+                    console.log("Backup time saved successfully!");
+                } else {
+                    console.error("Failed to save backup time");
+                }
+            } catch (error) {
+                console.error("Error:", error);
+            }
         }
     };
 
@@ -25,7 +48,7 @@ const Backup = () => {
 
     return (
         <div className="backup-block w-80 mx-auto p-4">
-            <div className="mb-4">
+            <div className="">
                 <label htmlFor="backup-toggle" className="flex items-center">
                     <input
                         id="backup-toggle"
@@ -37,7 +60,7 @@ const Backup = () => {
                     Turn on backup
                 </label>
             </div>
-            <section className={`backup-time-select ${isBackupOn ? '' : 'opacity-50'}`}>
+            <section className={`backup-time-select m-3 ${isBackupOn ? '' : 'opacity-50'}`}>
                 <label htmlFor="backup-time">Select a backup hour: </label>
                 <select
                     id="backup-time"
@@ -52,14 +75,39 @@ const Backup = () => {
                             {time}
                         </option>
                     ))}
+                    {/* <option value='custom-time'>Custom Time</option> */}
                 </select>
             </section>
-            <div className="mb-4 p-5 m-3">
+            <div className="m-3 w-80 backup-password">
+                <label htmlFor="setup-password" >
+                    <input
+                        id='setup-password'
+                        type='checkbox'
+                        checked={backupPassword}
+                        disabled={!isBackupOn}
+                        onChange={() => setBackupPassword(!backupPassword)}
+                        className='mr-2'
+                    />
+                    Set a password for the backup?
+                </label>
+                {backupPassword && (
+                    <input
+                        type='password'
+                        id='password'
+                        placeholder='Enter backup password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        className='w-full m-2 border rounded'
+                    />
+                )}
+            </div>
+            <div className="w-80 m-3">
                 <label htmlFor="send-confirmation" className="flex items-center">
                     <input
                         id="send-confirmation"
                         type="checkbox"
                         checked={sendConfirmation}
+                        disabled={!isBackupOn}
                         onChange={() => setSendConfirmation(!sendConfirmation)}
                         className="mr-2"
                     />
@@ -72,13 +120,15 @@ const Backup = () => {
                         placeholder="Enter email address"
                         value={email}
                         onChange={(e) => setEmail(e.target.value)}
-                        className="w-full p-2 border rounded"
+                        className="w-full m-2 border rounded"
                     />
                 )}
             </div>
             <div className="text-center">
                 <button
-                    className={`bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700 ${isBackupOn ? '' : 'cursor-not-allowed'}`}
+                    className={` px-4 py-2 rounded ${isBackupOn
+                        ? 'bg-blue-500  hover:bg-blue-700'
+                        : 'cursor-not-allowed bg-gray-400'}`}
                     onClick={handleNewTime}
                     disabled={!isBackupOn}
                 >
