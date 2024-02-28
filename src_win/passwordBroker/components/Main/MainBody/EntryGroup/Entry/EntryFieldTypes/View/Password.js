28aa589def4ce9e2623f6fe9f516fcc1577376c8
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-02-28 12:08:16 +0300

    Add copy-to-clipboard functionality and update application version
    This commit introduces a "CopyToClipboard" component that allows users to copy different kinds of information displayed in the application, such as notes, passwords, and links. This will improve the user experience by simplifying the process of copying and pasting these details. The application version in 'Package.appxmanifest' has also been updated.

--- src_win/passwordBroker/components/Main/MainBody/EntryGroup/Entry/EntryFieldTypes/View/Password.js
+++ src_win/passwordBroker/components/Main/MainBody/EntryGroup/Entry/EntryFieldTypes/View/Password.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import {Text, View} from 'react-native-windows';
+import React, {useRef} from 'react';
+import {Text, TouchableOpacity, TouchableWithoutFeedback, View} from 'react-native-windows';
 import tw from 'twrnc';
+import CopyToClipboard from './CopyToClipboard';
 
 const Password = ({login, value}) => {
     return (
         <View style={tw`flex flex-row flex-wrap p-0 m-0`}>
-            <View style={tw`px-2 flex flex-row flex-wrap`}>
+            <CopyToClipboard value={login} style={tw`px-2`}>
                 <Text style={tw`text-slate-400 pr-3`}>login: </Text>
                 <Text>{login}</Text>
-            </View>
-            <View style={tw`px-2 flex flex-row flex-wrap`}>
-                <Text style={value === '' ? tw`hidden` : tw`text-slate-400 pr-3`}>password: </Text>
+            </CopyToClipboard>
+            <CopyToClipboard value={value} style={tw`px-2`}>
+                <Text style={value === '' ? tw`hidden` : tw`text-slate-400 pr-3`}>pass: </Text>
                 <Text>{value}</Text>
-            </View>
+            </CopyToClipboard>
         </View>
     );
 };
