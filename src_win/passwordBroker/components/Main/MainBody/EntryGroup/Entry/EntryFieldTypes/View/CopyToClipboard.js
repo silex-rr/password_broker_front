ae1216f2f37ed588f1787662065af2f20c3be6a7
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-02-28 12:15:19 +0300

    Add copy-to-clipboard functionality and update application version
    This commit introduces a "CopyToClipboard" component that allows users to copy different kinds of information displayed in the application, such as notes, passwords, and links. This will improve the user experience by simplifying the process of copying and pasting these details. The application version in 'Package.appxmanifest' has also been updated.

---
+++ src_win/passwordBroker/components/Main/MainBody/EntryGroup/Entry/EntryFieldTypes/View/CopyToClipboard.js
@@ -0,0 +1,34 @@
+import React, {useContext, useRef} from 'react';
+import * as Animatable from 'react-native-animatable';
+import tw from 'twrnc';
+import {Text, TouchableWithoutFeedback, View} from 'react-native-windows';
+import AppContext from '../../../../../../../../AppContext';
+
+const CopyToClipboard = ({value, style, children}) => {
+    const {copyToClipboard} = useContext(AppContext);
+    const refTarget = useRef();
+    const copyClickHandler = () => {
+        if (value === '') {
+            return;
+        }
+        copyToClipboard(value);
+        refTarget.current.setNativeProps({style: {opacity: 1}});
+        setTimeout(() => {
+            refTarget.current.duration = 1500;
+            refTarget.current.animate('fadeOut');
+        }, 300);
+    };
+
+    return (
+        <TouchableWithoutFeedback onPress={copyClickHandler}>
+            <View style={{...style, ...tw`flex flex-row flex-wrap`}}>
+                <Animatable.View ref={refTarget} style={tw`opacity-0 absolute left--13 bg-sky-200 rounded px-1`}>
+                    <Text style={tw`text-slate-800`}>copied</Text>
+                </Animatable.View>
+                {children}
+            </View>
+        </TouchableWithoutFeedback>
+    );
+};
+
+export default CopyToClipboard;
