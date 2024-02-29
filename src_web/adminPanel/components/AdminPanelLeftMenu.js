530a483a7cd568279b49e9bfc38a7b30f4592fc2
Polina <polina@blsv.space>
2024-02-29 17:37:23 +0400

    style and menu block was revamped.

--- src_web/adminPanel/components/AdminPanelLeftMenu.js
+++ src_web/adminPanel/components/AdminPanelLeftMenu.js
@@ -1,26 +1,27 @@
 import AdminPanelLeftMenuNagivation from './AdminPanelLeftMenuNavigation';
 
 const AdminPanelLeftMenu = () => {
-    const title = 'Admin panel elements';
+    const title = 'Admin panel';
+    const buttonStyle = "w-full p-2 text-left hover:text-black hover:bg-slate-400"
 
     return (
-        <section className="bg-slate-900 portrait:w-full">
-            <div className="basis-1/4 pr-1 text-slate-400">
+        <section className="bg-slate-900 flex flex-col portrait:flex-row portrait:w-full">
+            <div className="">
                 <div className="w-full bg-slate-200 px-3 py-1 text-slate-800">{title}</div>
             </div>
-            <ul className="menu flex flex-col portrait:flex-row">
-                <li className="relative px-2 py-2">
+            <ul className="flex flex-col gap-3 portrait:flex-row">
+                <button className={buttonStyle}>
                     <AdminPanelLeftMenuNagivation path={'/admin/user_control/'} name={'User Control'} />
-                </li>
-                <li className="relative px-2 py-2">
+                </button>
+                <button className={buttonStyle}>
                     <AdminPanelLeftMenuNagivation path={'/admin/settings/'} name={'Settings'} />
-                </li>
-                <li className="relative px-2 py-2">
+                </button>
+                <button className={buttonStyle}>
                     <AdminPanelLeftMenuNagivation path={'/admin/logs/'} name={'Logs'} />
-                </li>
-                <li className='relative px-2 py-2'>
+                </button>
+                <button className={buttonStyle}>
                     <AdminPanelLeftMenuNagivation path={'/admin/backup/'} name={'Backup'} />
-                </li>
+                </button>
             </ul>
         </section>
     );
