d2e460a1b6d214b9b94ca943c8bb19e5857a6b4a
Anton.b <46520333+silex-rr@users.noreply.github.com>
2024-03-06 16:56:04 +0300

    Implement jsotp library for TOTP support
    The jsotp library has been added to the project to support Time-based One-time Passwords (TOTPs). A TOTP component was created in EntryFieldTypes to generate and handle these passwords. Now, users can effortlessly input an OTP secret key in edit mode and copy the generated TOTP in view mode.

--- src_web/passwordBroker/components/MainBody/EntryGroup/EntryFieldTypes/View/TOTP.js
+++ src_web/passwordBroker/components/MainBody/EntryGroup/EntryFieldTypes/View/TOTP.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import CopyToClipboard from './CopyToClipboard';
+import {TOTP as TOTPJS} from 'jsotp';
 
 const TOTP = ({value}) => {
+    const [token, setToken] = useState('');
+
+    useEffect(() => {
+        if (value) {
+            const totp = new TOTPJS(value);
+            setToken(totp.now());
+            console.log(token);
+        }
+    }, [value]);
+
     return (
         <div className="col-span-3 basis-1/2 px-2">
             <CopyToClipboard value={value}>
                 <div className="whitespace-pre-line">{value}</div>
             </CopyToClipboard>
+            <CopyToClipboard>{token}</CopyToClipboard>
         </div>
     );
 };
