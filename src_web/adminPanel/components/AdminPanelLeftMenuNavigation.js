798f3c123dc8fa8f2004c10bd60ec2f49690003e
Polina <polina@blsv.space>
2024-03-22 15:45:26 +0400

    added handing selectedItem, added styling.

--- src_web/adminPanel/components/AdminPanelLeftMenuNavigation.js
+++ src_web/adminPanel/components/AdminPanelLeftMenuNavigation.js
@@ -1,15 +1,19 @@
-import {useNavigate} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const AdminPanelLeftMenuNagivation = props => {
+    console.log(props)
     const navigate = useNavigate();
     const path = props.path;
     const name = props.name;
+    const selectedItem = props.selectedItem
+
+    const selectedMenuItem = path === selectedItem ? 'font-bold pl-2' : ''
 
     const handleNavigation = path => {
         return navigate(path);
     };
     return (
-        <h3
+        <h3 className={`${selectedMenuItem}`}
             onClick={() => {
                 handleNavigation(path);
             }}>
