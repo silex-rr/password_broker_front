1736e5d506eb91042064429774dac0edd633a0db
Anton.be <anton.be@itechpsp.com>
2025-03-06 11:36:07 +0300

    PBD-43: Bulk Entry delete completed

--- src_shared/passwordBroker/contexts/EntryBulkEditContextProvider.js
+++ src_shared/passwordBroker/contexts/EntryBulkEditContextProvider.js
@@ -3,10 +3,12 @@ import EntryBulkEditContext from './EntryBulkEditContext';
 import axios from 'axios';
 import PasswordBrokerContext from './PasswordBrokerContext';
 import {ENTRY_GROUP_REQUIRED_LOADING} from '../constants/EntryGroupStatus';
+import GlobalContext from '../../common/contexts/GlobalContext';
 
 const EntryBulkEditContextProvider = props => {
     const [massSelectorChecked, setMassSelectorChecked] = useState(false);
     const [checkedEntries, setCheckedEntries] = useState([]);
+    const {logActivityManual} = useContext(GlobalContext);
     const {baseUrl, entryGroupId, setEntryGroupStatus} = useContext(PasswordBrokerContext);
 
     const moveEntries = targetEntryGroupId => {
@@ -22,17 +24,24 @@ const EntryBulkEditContextProvider = props => {
             });
     };
 
-    const deleteEntries = () => {
+    const deleteEntriesRequest = () => {
         axios
             .post(`${baseUrl}/entryGroups/${entryGroupId}/entries/bulkEdit/delete`, {
                 entries: checkedEntries,
             })
             .then(_ => {
                 setEntryGroupStatus(ENTRY_GROUP_REQUIRED_LOADING);
+                setCheckedEntries([]);
+                setMassSelectorChecked(false);
+                logActivityManual('Entries deleted');
             })
             .catch(error => {
-                console.log(error);
+                console.log(error.message ?? 'Something went wrong, try again');
             });
+    };
+
+    const deleteEntries = () => {
+        deleteEntriesRequest();
     };
 
     return (
